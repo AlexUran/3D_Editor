5071000e7db3c2407acf468d5d4ad94572e39461
alex3453 <sokolov.uran@yandex.ru>
2021-12-17 00:51:54 +0500

    нда

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
+import pygame as pg
+from pygame.locals import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 
-# Press the green button in the gutter to run the script.
+def draw_cube():
+    p = [
+        (1, 1, -1),
+        (1, -1, -1),
+        (-1, -1, -1),
+        (-1, 1, -1),
+        (1, 1, 1),
+        (1, -1, 1),
+        (-1, -1, 1),
+        (-1, 1, 1)
+    ]
+    edges = [
+        (0, 1),
+        (1, 2),
+        (2, 3),
+        (3, 0),
+        (4, 5),
+        (5, 6),
+        (6, 7),
+        (7, 4),
+        (0, 4),
+        (1, 5),
+        (2, 6),
+        (3, 7),
+    ]
+    glBegin(GL_LINES)
+    glVertex3fv((0, 0, 0))
+    glVertex3fv((2, 0, 0))
+    glVertex3fv((0, 0, 0))
+    glVertex3fv((0, 2, 0))
+    glVertex3fv((0, 0, 0))
+    glVertex3fv((0, 0, 2))
+    for edge in edges:
+        glVertex3fv(p[edge[0]])
+        glVertex3fv(p[edge[1]])
+    glEnd()
+
+def main():
+    pg.init()
+    display = (800, 600)
+    pg.display.set_mode(display, DOUBLEBUF | OPENGL)
+    clock = pg.time.Clock()
+
+    gluPerspective(45, display[0] / display[1], 0.1, 50.0)
+
+    glTranslatef(0, 0, -10)
+
+    run = True
+    while run:
+        clock.tick(60)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+        glRotate(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        draw_cube()
+        pg.display.flip()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
